30878c71ab6a6f486d0c30b8243935bc3276421e
Денис Капустин <kapustin754@gmail.com>
2021-05-23 23:52:50 +0300

    159753

--- KorRegr/Form1.Designer.cs
+++ KorRegr/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -51,6 +52,18 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
@@ -75,7 +88,7 @@
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.ShowCellToolTips = false;
-            this.dataGridView1.Size = new System.Drawing.Size(863, 251);
+            this.dataGridView1.Size = new System.Drawing.Size(433, 251);
             this.dataGridView1.TabIndex = 0;
             // 
             // Column1
@@ -123,7 +136,7 @@
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(118, 79);
+            this.linkLabel1.Location = new System.Drawing.Point(62, 34);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(50, 13);
             this.linkLabel1.TabIndex = 1;
@@ -133,7 +146,7 @@
             // linkLabel2
             // 
             this.linkLabel2.AutoSize = true;
-            this.linkLabel2.Location = new System.Drawing.Point(397, 79);
+            this.linkLabel2.Location = new System.Drawing.Point(357, 34);
             this.linkLabel2.Name = "linkLabel2";
             this.linkLabel2.Size = new System.Drawing.Size(88, 13);
             this.linkLabel2.TabIndex = 2;
@@ -143,7 +156,7 @@
             // linkLabel3
             // 
             this.linkLabel3.AutoSize = true;
-            this.linkLabel3.Location = new System.Drawing.Point(680, 79);
+            this.linkLabel3.Location = new System.Drawing.Point(676, 34);
             this.linkLabel3.Name = "linkLabel3";
             this.linkLabel3.Size = new System.Drawing.Size(69, 13);
             this.linkLabel3.TabIndex = 3;
@@ -154,14 +167,18 @@
             // 
             chartArea1.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea1);
-            this.chart1.Location = new System.Drawing.Point(12, 457);
+            this.chart1.Location = new System.Drawing.Point(451, 313);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
             series1.Name = "Series1";
             series1.YValuesPerPoint = 4;
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series2.Name = "Series2";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(279, 251);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(396, 290);
             this.chart1.TabIndex = 5;
             this.chart1.Text = "chart1";
             // 
@@ -179,10 +196,10 @@
             this.Column9,
             this.Column10});
             this.dataGridView2.EnableHeadersVisualStyles = false;
-            this.dataGridView2.Location = new System.Drawing.Point(297, 457);
+            this.dataGridView2.Location = new System.Drawing.Point(12, 352);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersVisible = false;
-            this.dataGridView2.Size = new System.Drawing.Size(578, 251);
+            this.dataGridView2.Size = new System.Drawing.Size(433, 251);
             this.dataGridView2.TabIndex = 6;
             // 
             // Column11
@@ -223,30 +240,143 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 362);
+            this.textBox1.Location = new System.Drawing.Point(660, 95);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 7;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(139, 361);
+            this.textBox2.Location = new System.Drawing.Point(660, 121);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 20);
             this.textBox2.TabIndex = 8;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(292, 363);
+            this.textBox3.Location = new System.Drawing.Point(660, 147);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(100, 20);
             this.textBox3.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(454, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Среднее квадр. откл. для признака Х:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(454, 124);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Среднее квадр. откл. для признака Y:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(454, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(197, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Линейный коэффициент корреляции:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(454, 176);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Средняя ощибка коэф. корреляции:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(660, 173);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(459, 202);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(233, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Проверка коэф. корреляции на значимость:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(698, 199);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(459, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "t-табл:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(504, 228);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 18;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(454, 257);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "label7";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(454, 280);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 13);
+            this.label8.TabIndex = 20;
+            this.label8.Text = "Коэф. Спирмена";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(551, 277);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 20);
+            this.textBox7.TabIndex = 21;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(887, 721);
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(887, 877);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
@@ -288,6 +418,18 @@
         public System.Windows.Forms.TextBox textBox1;
         public System.Windows.Forms.TextBox textBox2;
         public System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        public System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label5;
+        public System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label6;
+        public System.Windows.Forms.TextBox textBox6;
+        public System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        public System.Windows.Forms.TextBox textBox7;
     }
 }
 
--- KorRegr/MDIParent1.cs
+++ KorRegr/MDIParent1.cs
@@ -125,10 +125,6 @@ namespace KorRegr
                             kk++;
                         }
 
-                    //for (int i = 0; i < DlyaRangN.Length; i++)
-                    //    for (int j = 0; j < DlyaRangN.Length; j++)
-                    //        if ((double)childForm.dataGridView1[y + 1, j].Value == DlyaRangN[i])
-                    //            childForm.dataGridView2.Rows[j].Cells[y + 2].Value = i + 1;
 
                     for (int i = 0; i < DlyaRangN.Length; i++) {
                         double row = 0, kol = 0;
@@ -141,6 +137,7 @@ namespace KorRegr
                         for (int j = 0; j < SortMas.Length; j++)
                             if (DlyaRangN[i] == (double)childForm.dataGridView1[y + 1, j].Value)
                                 childForm.dataGridView2.Rows[j].Cells[y + 2].Value = Math.Round(row / kol, 1);
+
                     }
                     
                 }
@@ -148,11 +145,68 @@ namespace KorRegr
                 SortAndRang(0); // для Nx
                 SortAndRang(1); // для Ny
 
+
+                //var a0 = Math.Round((SumY * SumX2 - SumXY * SumX) / (n - SumX2 - Math.Pow(SumX, 2)), 2);
+                //var a1 = Math.Round((n * SumXY - SumX * SumY) / n * SumX2 - Math.Pow(SumX2, 2), 2);
+                var a0 = Math.Round((sred(SumY) * sred(SumX2) - sred(SumXY) * sred(SumX)) / (n - sred(SumX2) - Math.Pow(SumX, 2)),2);
+                var a1 = Math.Round((n * sred(SumXY) - sred(SumX) * sred(SumY)) / n * sred(SumX2) - Math.Pow(sred(SumX2), 2), 2);
+
+                var SortMasX = new double[n];
+                for (int i = 0; i < n; i++)
+                    SortMasX[i] = childForm.d[i, 0];
+
+                var Tochki = new double[SortMasX.Length];
+                int kkk = 0;
+
+                Array.Sort(SortMasX);
+                Array.Reverse(SortMasX);
+
+                for (int i = 1; i < SortMasX.Length; i++)
+                    if (SortMasX[i] != SortMasX[i - 1])
+                    {
+                        var t = SortMasX[i];
+                        Tochki[kkk] = SortMasX[i - 1];
+                        Tochki[kkk + 1] = t;
+                        kkk++;
+                    }
+
+                for (int i = 0; i < Tochki.Length; i++)
+                {
+                    double X = Tochki[i];
+                    double notYx = a0 + a1 * X;//прямая
+                    //double notYx = a0 + a1 * (1 / x);//гипербола
+                    //double notYx = a0 * Math.Pow(a1 , x);//показательная функция
+
+                    childForm.chart1.Series[1].Points.AddXY(Tochki[i], notYx);
+                }
+
+                double symma = 0;
+
                 for (int i = 0; i < n; i++)
                 {
                     childForm.dataGridView2.Rows[i].Cells[4].Value = Convert.ToDouble(childForm.dataGridView2.Rows[i].Cells[2].Value) - Convert.ToDouble(childForm.dataGridView2.Rows[i].Cells[3].Value);
                     childForm.dataGridView2.Rows[i].Cells[5].Value = Math.Pow(Convert.ToDouble(childForm.dataGridView2.Rows[i].Cells[4].Value), 2);
+                    symma += Convert.ToDouble(childForm.dataGridView2.Rows[i].Cells[5].Value);
                 }
+                childForm.dataGridView2.Rows.Add("n = " + n, null, null, null, "Сумма", symma);
+
+                //расчеты корреляции
+                childForm.textBox4.Text = (n > 50 && !(n < 30)) ? Convert.ToString(Math.Round((1 - Math.Pow(Convert.ToDouble(childForm.textBox3.Text), 2)) / Math.Sqrt(n), 2))
+                    : Convert.ToString(Math.Round(Math.Sqrt(1 - Math.Pow(Convert.ToDouble(childForm.textBox3.Text), 2)) / Math.Sqrt(n - 2), 2));;
+                childForm.textBox5.Text = Convert.ToString(Math.Round(Math.Abs(Convert.ToDouble(childForm.textBox3.Text)) / Convert.ToDouble(childForm.textBox4.Text), 2));
+
+                var r = Math.Abs(Convert.ToDouble(childForm.textBox3.Text));
+
+
+                var svyaz = (r == 0) ? "отсутствует" : (0 < r && r < 0.3) ? "слабая" : (0.3 <= r && r <= 0.7) ? "средней силы" : "сильная";
+
+                childForm.label7.Text = "Связь между признаками х и у: " + svyaz;
+                var df = n - 2;
+                //childForm.textBox6.Text = Convert.ToString();
+
+                //после рангов
+                childForm.textBox7.Text = Convert.ToString(Math.Round(1-((6*symma)/(n*(Math.Pow(n, 2)-1))),2));
+
 
                 saveFileDialog1.FileName = openFileDialog1.FileName;
                 namefile = openFileDialog1.SafeFileName;
